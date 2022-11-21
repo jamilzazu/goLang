d7fa6496772aeba9716cf3648ea1385a816af71e
Jamil Zazu <jamilzazu@gmail.com>
2022-11-20 20:31:12 -0400

    feat: use structs composite

---
+++ 01-Foundation/12 - Structs Composite/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	AddressName string
+	Number      int
+	City        string
+	State       string
+}
+
+type Client struct {
+	Name   string
+	Age    int
+	Active bool
+	Address
+}
+
+func main() {
+	client := BuildCClient()
+	client.Address = BuildCompositeAddress(client)
+	RunPrint(client)
+}
+
+func BuildCClient() Client {
+	client := Client{
+		Name:   "Jamil",
+		Age:    34,
+		Active: true,
+	}
+	return client
+}
+
+func BuildCompositeAddress(client Client) Address {
+	client.Address.AddressName = "Rua A" // or client.AddressName
+	client.Address.Number = 10           // or client.Number
+	client.Address.City = "São Paulo"    // or client.City
+	client.Address.State = "SP"          // or client.State
+	return client.Address
+}
+
+func RunPrint(client Client) {
+	fmt.Printf("Name: %s \n", client.Name)
+	fmt.Printf("Age: %d \n", client.Age)
+	fmt.Printf("Active: %t \n", client.Active)
+	fmt.Printf("Address: %s \n", client.Address.AddressName)
+	fmt.Printf("Number: %d \n", client.Address.Number)
+	fmt.Printf("City: %s \n", client.Address.City)
+	fmt.Printf("State: %s \n", client.Address.State)
+}
