2b085e190c4a80524c1bcec5ae1482e5903580ec
Jamil Zazu <jamilzazu@gmail.com>
2022-11-21 19:02:57 -0400

    feat: use generic with new type

--- 01-Foundation/19 - Generics/main.go
+++ 01-Foundation/19 - Generics/main.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+type NewInt int
+
 type TypeNumber interface {
-	int | float64
+	~int | ~float64 // use ~ use to accept a custom type such as NewInt
 }
 
 func ConstrainedSum[T TypeNumber](values map[string]T) T {
@@ -30,6 +32,10 @@ func getMapFloat() map[string]float64 {
 	return map[string]float64{"Jamil": 1000.05, "Cristiano": 2000.10, "Flavio": 3000.20}
 }
 
+func getMapNewInt() map[string]NewInt {
+	return map[string]NewInt{"Jamil": 1001, "Cristiano": 2001, "Flavio": 3001}
+}
+
 func main() {
 	fmt.Println("\nUsing unconstrained sum")
 	fmt.Printf("Int %d \n", Sum(getMapInt()))
@@ -38,5 +44,8 @@ func main() {
 	fmt.Println("\nUsing constrained sum")
 	fmt.Printf("Int %d \n", ConstrainedSum(getMapInt()))
 	fmt.Printf("Float %.2f \n", ConstrainedSum(getMapFloat()))
+
+	fmt.Println("\nUsing constrained sum with custom type")
+	fmt.Printf("NewInt %d \n", ConstrainedSum(getMapNewInt()))
 
 }
