c3e3bcdda7f8a73aa8606946e0174f13ccc7ec52
Jamil Zazu <jamilzazu@gmail.com>
2022-11-21 21:31:53 -0400

    refact: use loop

--- 01-Foundation/21 - Loop/main.go
+++ 01-Foundation/21 - Loop/main.go
@@ -1,18 +1,19 @@
 package main
 
 func main() {
+	useSimpleFor()
+	useRangeFor()
+	useInfiniteFor()
+}
+
+func useSimpleFor() {
 	println("use simple for")
 	for i := 0; i < 4; i++ {
 		println(i)
 	}
+}
 
-	println("\nuse simple for similar to while")
-	i := 0
-	for i < 2 {
-		println(i)
-		i++
-	}
-
+func useRangeFor() {
 	numbers := []string{"one", "two", "three"}
 	println("\nuse range")
 	for key, value := range numbers {
@@ -23,7 +24,9 @@ func main() {
 	for _, value := range numbers {
 		println(value)
 	}
+}
 
+func useInfiniteFor() {
 	println("\ninfinite loop")
 	for {
 		println("infinite loop")
