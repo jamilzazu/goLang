0f7ff701304e2ac6d937a6e73e7e213e3447ceb4
Jamil Zazu <jamilzazu@gmail.com>
2022-11-22 20:34:19 -0400

    feat: http call get

---
+++ 02 - Important Packages/02 - Making HTTP Call/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func requestGet() {
+	request := createRequestGet()
+	readRequestGet(request)
+	closeRequestBody(request)
+}
+
+func createRequestGet() *http.Response {
+	request, err := http.Get("https://google.com")
+	messageError(err)
+	return request
+}
+
+func readRequestGet(request *http.Response) {
+	result, err := io.ReadAll(request.Body)
+	messageError(err)
+	println(string(result))
+}
+
+func closeRequestBody(request *http.Response) {
+	request.Body.Close()
+}
+
+func messageError(message error) {
+	if message != nil {
+		panic(message)
+	}
+}
+
+func main() {
+	requestGet()
+}
