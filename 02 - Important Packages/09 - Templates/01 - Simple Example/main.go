37d3b727d58c5616229a2a13ce488969eea03ef8
Jamil Zazu <jamilzazu@gmail.com>
2022-11-24 19:42:55 -0400

    feat: create new template

---
+++ 02 - Important Packages/09 - Templates/01 - Simple Example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Client struct {
+	Name string
+	Age  int
+}
+
+func createClient() Client {
+	return Client{"Jamil", 34}
+}
+
+func newTemplate(templateName string) *template.Template {
+	return template.New(templateName)
+}
+
+func executeClientTemplate() {
+	newTemplate := newTemplate("ClientTemplate")
+	newTemplate, _ = newTemplate.Parse("Client: {{.Name}} - Age: {{.Age}}")
+	err := newTemplate.Execute(os.Stdout, createClient())
+	messageError(err)
+}
+
+func messageError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	executeClientTemplate()
+}
