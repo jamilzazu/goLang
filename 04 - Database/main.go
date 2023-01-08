dec5fe74ba5e37910f57541a8ba7a494cc451a26
Jamil Zazu <jamilzazu@gmail.com>
2023-01-08 19:21:24 -0400

    feat: updating database data

--- 04 - Database/main.go
+++ 04 - Database/main.go
@@ -20,6 +20,19 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 
+func insertNewProduct(db *sql.DB, err error) *Product {
+	product := NewProduct("NoteBook", 1899.90)
+	err = insertProduct(db, product)
+	messageError(err)
+	return product
+}
+
+func updateNewProduct(db *sql.DB, product *Product, err error) {
+	product.Price = 201.0
+	err = updateProduct(db, product)
+	messageError(err)
+}
+
 func insertProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("insert into  products(id, name, price) values (?, ?, ?)")
 	messageError(err)
@@ -31,6 +44,27 @@ func insertProduct(db *sql.DB, product *Product) error {
 	return nil
 }
 
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	messageError(err)
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	messageError(err)
+
+	return nil
+}
+
+func OpenConnectionMySql() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/godb")
+	messageError(err)
+	return db, err
+}
+
+func closeConnectionMySql(db *sql.DB) {
+	defer db.Close()
+}
+
 func messageError(err error) {
 	if err != nil {
 		panic(err)
@@ -38,12 +72,8 @@ func messageError(err error) {
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/godb")
-	messageError(err)
-	defer db.Close()
-	product := NewProduct("NoteBook", 1899.90)
-
-	err = insertProduct(db, product)
-	messageError(err)
-
+	db, err := OpenConnectionMySql()
+	product := insertNewProduct(db, err)
+	updateNewProduct(db, product, err)
+	closeConnectionMySql(db)
 }
