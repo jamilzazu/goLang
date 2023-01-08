498fc5a97358cb5dacdbfa55bf89adf8fd2207c6
Jamil Zazu <jamilzazu@gmail.com>
2023-01-08 19:05:47 -0400

    feat: inserting data into the database

--- 04 - Database/main.go
+++ 04 - Database/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -16,6 +20,30 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into  products(id, name, price) values (?, ?, ?)")
+	messageError(err)
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	messageError(err)
+
+	return nil
+}
+
+func messageError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/godb")
+	messageError(err)
+	defer db.Close()
+	product := NewProduct("NoteBook", 1899.90)
+
+	err = insertProduct(db, product)
+	messageError(err)
 
 }
