6eea4b5b41ad45dfd39e1574239cc58c2a537a66
Jamil Zazu <jamilzazu@gmail.com>
2023-01-08 19:38:49 -0400

    feat: select product by id using queryRow

--- 04 - Database/main.go
+++ 04 - Database/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
@@ -20,20 +21,38 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 
+func selectProduct(db *sql.DB, productId string, err error) {
+	productSelected, err := selectProductByIdMySql(db, productId)
+	messageError(err)
+	fmt.Printf("Product: %v, has the price of %.2f", productSelected.Name, productSelected.Price)
+}
+
 func insertNewProduct(db *sql.DB, err error) *Product {
 	product := NewProduct("NoteBook", 1899.90)
-	err = insertProduct(db, product)
+	err = insertProductMySql(db, product)
 	messageError(err)
 	return product
 }
 
 func updateNewProduct(db *sql.DB, product *Product, err error) {
 	product.Price = 201.0
-	err = updateProduct(db, product)
+	err = updateProductMySql(db, product)
 	messageError(err)
 }
 
-func insertProduct(db *sql.DB, product *Product) error {
+func selectProductByIdMySql(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	messageError(err)
+	defer stmt.Close()
+
+	var product Product
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price)
+	messageError(err)
+
+	return &product, nil
+}
+
+func insertProductMySql(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("insert into  products(id, name, price) values (?, ?, ?)")
 	messageError(err)
 	defer stmt.Close()
@@ -44,7 +63,7 @@ func insertProduct(db *sql.DB, product *Product) error {
 	return nil
 }
 
-func updateProduct(db *sql.DB, product *Product) error {
+func updateProductMySql(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
 	messageError(err)
 	defer stmt.Close()
@@ -75,5 +94,6 @@ func main() {
 	db, err := OpenConnectionMySql()
 	product := insertNewProduct(db, err)
 	updateNewProduct(db, product, err)
+	selectProduct(db, product.ID, err)
 	closeConnectionMySql(db)
 }
