869f0d1a60c8bf25e6fd120d6a7e2e83750c65ba
Jamil Zazu <jamilzazu@gmail.com>
2023-01-09 12:42:46 -0400

    test: fuzzing

--- 06 - Testing/01 - Starting/rate_test.go
+++ 06 - Testing/01 - Starting/rate_test.go
@@ -16,6 +16,11 @@ import "testing"
 // go test -bench . -run=^# -count=10 -benchtime=3s
 // go test -bench . -run=^# -benchmem
 
+// Fuzz
+// go test -fuzz=.
+// go test -fuzz=. -run=^#
+// go test -fuzz=. -fuzztime=5s -run=^#
+
 func TestCalculateRate(t *testing.T) {
 	amount := 500.0
 	expected := 5.0
@@ -59,4 +64,24 @@ func BenchmarkCalculateRateBatchTimeSleep(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		CalculateRateTimeSleep(500.0)
 	}
+}
+
+func FuzzCalculateRate(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, -500.0, 1000.0, 1500.0, 1689.92}
+
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateRateFuzz(amount)
+
+		if amount <= 0 && result != 0 {
+			t.Errorf("Reveived %f but expected 0", result)
+		}
+
+		if amount > 20000 && result != 20 {
+			t.Errorf("Reveived %f but expected 20", result)
+		}
+	})
 }
